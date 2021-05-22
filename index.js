a0bad42197299358ed0fc4d8cf784b1426260f34
Maria Florencia <auka83@gmail.com>
2021-05-22 20:26:24 -0300

    commit com .env

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.get('/',  async(req, res) => {
     //tirando diretamente o data do res.data
     const { data } = await axios('https://api.github.com/users/takenet/repos')
    
-    console.log(data)
+    //console.log(data)
 
     return res.send(data)
     //return res.json(data)
 })    
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 
